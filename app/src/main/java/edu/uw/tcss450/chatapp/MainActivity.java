21b0a3bb6a99fd2ee4d8d0f31665d74eec7b4d60
taehong3 <73399332+taehong3@users.noreply.github.com>
2021-04-20 15:07:17 -0700

    Taehong gitahub testing

--- app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
@@ -16,4 +16,5 @@ public class MainActivity extends AppCompatActivity {
     // Seungku Kim Testing pushing
     // Sample pull request comment
     // Seungku Kim Testing Again with Branch
+    // Taehong Kim testing again with branch
 }
