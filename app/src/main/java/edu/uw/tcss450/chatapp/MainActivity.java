3535636f9aba16d590a929d6c18256a2b3d3c435
taehong3 <73399332+taehong3@users.noreply.github.com>
2021-04-20 14:21:25 -0700

    test git

--- app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //testing
     //Taehong Kim Testing pushing
     // Seungku Kim Testing pushing
 }
