ee0a14a25485ee97d2ffd12115f0cde595e8e0b8
Aman Gedefaw <aman44g@gmail.com>
2021-05-06 05:10:37 -0700

    added navigation up

--- app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp/MainActivity.java
@@ -30,4 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
 }
