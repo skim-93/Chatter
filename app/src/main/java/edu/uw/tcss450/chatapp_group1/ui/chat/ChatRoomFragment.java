e277c2f50df97f81cdde00bc1be9eaff62c8cc0d
Joseph Yun <39817074+josephyunzz@users.noreply.github.com>
2021-05-16 15:50:54 -0700

    Configure ChatRoomFragment to be able to send and receive messages in chat rooms

--- app/src/main/java/edu/uw/tcss450/chatapp_group1/ui/chat/ChatRoomFragment.java
+++ app/src/main/java/edu/uw/tcss450/chatapp_group1/ui/chat/ChatRoomFragment.java
@@ -1,26 +1,98 @@
 package edu.uw.tcss450.chatapp_group1.ui.chat;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
 import edu.uw.tcss450.chatapp_group1.R;
+import edu.uw.tcss450.chatapp_group1.databinding.FragmentChatRoomBinding;
+import edu.uw.tcss450.chatapp_group1.model.UserInfoViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
- * create an instance of this fragment.
  */
 public class ChatRoomFragment extends Fragment {
 
+    //The chat ID for "global" chat
+    private static final int HARD_CODED_CHAT_ID = 1;
+
+    private ChatViewModel mChatModel;
+    private UserInfoViewModel mUserModel;
+    private ChatSendViewModel mSendModel;
+
+    public ChatRoomFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+        mUserModel = provider.get(UserInfoViewModel.class);
+        mChatModel = provider.get(ChatViewModel.class);
+        mChatModel.getFirstMessages(HARD_CODED_CHAT_ID, mUserModel.getmJwt());
+        mSendModel = provider.get(ChatSendViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat_room, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FragmentChatRoomBinding binding = FragmentChatRoomBinding.bind(getView());
+
+        //SetRefreshing shows the internal Swiper view progress bar. Show this until messages load
+        binding.swipeContainer.setRefreshing(true);
+
+        final RecyclerView rv = binding.recyclerMessages;
+        //Set the Adapter to hold a reference to the list FOR THIS chat ID that the ViewModel
+        //holds.
+        rv.setAdapter(new ChatRecyclerViewAdapter(
+                mChatModel.getMessageListByChatId(HARD_CODED_CHAT_ID),
+                mUserModel.getEmail()));
+
+
+        //When the user scrolls to the top of the RV, the swiper list will "refresh"
+        //The user is out of messages, go out to the service and get more
+        binding.swipeContainer.setOnRefreshListener(() -> {
+            mChatModel.getNextMessages(HARD_CODED_CHAT_ID, mUserModel.getmJwt());
+        });
+
+        mChatModel.addMessageObserver(HARD_CODED_CHAT_ID, getViewLifecycleOwner(),
+                list -> {
+                    /*
+                     * This solution needs work on the scroll position. As a group,
+                     * you will need to come up with some solution to manage the
+                     * recyclerview scroll position. You also should consider a
+                     * solution for when the keyboard is on the screen.
+                     */
+                    //inform the RV that the underlying list has (possibly) changed
+                    rv.getAdapter().notifyDataSetChanged();
+                    rv.scrollToPosition(rv.getAdapter().getItemCount() - 1);
+                    binding.swipeContainer.setRefreshing(false);
+                });
+
+        //Send button was clicked. Send the message via the SendViewModel
+        binding.buttonSend.setOnClickListener(button -> {
+            mSendModel.sendMessage(HARD_CODED_CHAT_ID,
+                    mUserModel.getmJwt(),
+                    binding.editMessage.getText().toString());
+        });
+        //when we get the response back from the server, clear the edittext
+        mSendModel.addResponseObserver(getViewLifecycleOwner(), response ->
+                binding.editMessage.setText(""));
     }
 }
