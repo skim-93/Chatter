1f2772d4fc129db889bdfd2add5073d1ae904261
Seungku Kim <tmdrbtl1@uw.edu>
2021-05-26 15:36:31 -0700

    Update MainActivity.java
    Signout updates

--- app/src/main/java/edu/uw/tcss450/chatapp_group1/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp_group1/MainActivity.java
@@ -13,7 +13,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -138,5 +141,27 @@ public class MainActivity extends AppCompatActivity {
                 mModel.addMessage(intent.getIntExtra("chatid", -1), cm);
             }
         }
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drop_down, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_sign_out) {
+            signOut();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        SharedPreferences prefs = getSharedPreferences(
+                getString(R.string.keys_shared_prefs), Context.MODE_PRIVATE);
+        prefs.edit().remove(getString(R.string.keys_prefs_jwt)).apply();
+        //End the app completely
+        finishAndRemoveTask();
     }
 }
