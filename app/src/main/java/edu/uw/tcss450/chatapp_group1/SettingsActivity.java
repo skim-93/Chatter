554e436a8868da52597dd4f3e180f292ee478a6e
garret123h <gtullio12@protonmail.com>
2021-05-27 12:33:13 -1000

    Store theme change when new theme is selected in the Settings activity
    Why: When each activity is created then in the onCreate() method then
    check the SharedPreferences for theme to see what the theme should be.

--- app/src/main/java/edu/uw/tcss450/chatapp_group1/SettingsActivity.java
+++ app/src/main/java/edu/uw/tcss450/chatapp_group1/SettingsActivity.java
@@ -1,13 +1,13 @@
 package edu.uw.tcss450.chatapp_group1;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -44,6 +44,31 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            ListPreference themePref = (ListPreference) findPreference("theme");
+
+            themePref.setOnPreferenceChangeListener((preference, newValue) -> {
+
+                int index = themePref.findIndexOfValue(newValue.toString());
+
+                if (index == 1) { // Dark red theme
+                    SharedPreferences prefs = getActivity().getSharedPreferences(
+                            getString(R.string.shared_theme_pref),
+                            Context.MODE_PRIVATE
+                    );
+                    prefs.edit().putString(getString(R.string.theme_pref), "Dark Red").apply();
+                } else if (index == 0) { // Dark orange theme
+                    SharedPreferences prefs = getActivity().getSharedPreferences(
+                            getString(R.string.shared_theme_pref),
+                            Context.MODE_PRIVATE
+                    );
+                    prefs.edit().putString(getString(R.string.theme_pref), "Dark Orange").apply();
+                }
+                SharedPreferences prefs = getActivity().getSharedPreferences(
+                        getString(R.string.shared_theme_pref),
+                        Context.MODE_PRIVATE
+                );
+                return true;
+            });
         }
     }
 }
