70fb711460e6653ce54b5f87098144bd9eb2d6a3
gt <gt@pop-os.localdomain>
2021-05-31 18:13:33 -1000

    Update the zipcode when the user sets a new location from the marker on the map.
    Why: When the user clicks on the screen and creates a new marker than the user can click the "set location" button and in the background the zipcode of the weather list will update to that new location.

--- app/src/main/java/edu/uw/tcss450/chatapp_group1/ui/weather/LocationFragment.java
+++ app/src/main/java/edu/uw/tcss450/chatapp_group1/ui/weather/LocationFragment.java
@@ -2,6 +2,8 @@ package edu.uw.tcss450.chatapp_group1.ui.weather;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,9 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +24,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import edu.uw.tcss450.chatapp_group1.R;
 import edu.uw.tcss450.chatapp_group1.databinding.FragmentLocationBinding;
 
@@ -33,7 +37,11 @@ import edu.uw.tcss450.chatapp_group1.databinding.FragmentLocationBinding;
  */
 public class LocationFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener{
     private LocationViewModel mModel;
+    private WeatherListViewModel mWeatherModel;
     private GoogleMap mMap;
+    private double currentLatitude;
+    private double currentLongitude;
+    private String currentZipcode;
 
     public LocationFragment() {
         // Required empty public constructor
@@ -44,10 +52,13 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback, Go
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_location, container, false);
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         FragmentLocationBinding binding = FragmentLocationBinding.bind(getView());
+        mWeatherModel = new ViewModelProvider(getActivity())
+                .get(WeatherListViewModel.class);
         mModel = new ViewModelProvider(getActivity())
                 .get(LocationViewModel.class);
         mModel.addLocationObserver(getViewLifecycleOwner(), location ->
@@ -55,29 +66,36 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback, Go
         mModel.addLocationObserver(getViewLifecycleOwner(), location ->
                 binding.textLong.setText("longitude: " + String.valueOf(location.getLongitude())));
 
-        binding.buttonSetLocation.setOnClickListener(button ->
-                Navigation.findNavController(getView()).navigate(
-                        LocationFragmentDirections.actionLocationFragmentToNavigationWeather())
+        binding.buttonSetLocation.setOnClickListener(button -> {
+                    mWeatherModel.updateZipcode(currentZipcode);
+                }
         );
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         //add this fragment as the OnMapReadyCallback -> See onMapReady()
         mapFragment.getMapAsync(this);
-
     }
-
 
     @Override
     public void onMapClick(@NonNull LatLng latLng) {
-        Log.d("LAT/LONG", latLng.toString());
+        currentLatitude = latLng.latitude;
+        currentLongitude = latLng.longitude;
+        Geocoder geocoder = new Geocoder(getActivity(), Locale.ENGLISH);
+        try {
+            List<Address> addresses = geocoder.getFromLocation(currentLatitude, currentLongitude, 1);
+            Address returnAddress = addresses.get(0);
+            currentZipcode = returnAddress.getPostalCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Marker marker = mMap.addMarker(new MarkerOptions()
                 .position(latLng)
                 .title("New Marker"));
         mMap.animateCamera(
                 CameraUpdateFactory.newLatLngZoom(
                         latLng, mMap.getCameraPosition().zoom));
-
     }
 
     @Override
@@ -89,8 +107,6 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback, Go
         model.addLocationObserver(getViewLifecycleOwner(), location -> {
             if(location != null) {
                 googleMap.getUiSettings().setZoomControlsEnabled(true);
-//                googleMap.setMyLocationEnabled(true);
-
                 final LatLng c = new LatLng(location.getLatitude(), location.getLongitude());
                 //Zoom levels are from 2.0f (zoomed out) to 21.f (zoomed in)
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(c, 15.0f));
@@ -98,9 +114,8 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback, Go
         });
         mMap.setOnMapClickListener(this);
         mMap.setOnCameraMoveListener(this::onCameraMove);
-
-
     }
+
     private void onCameraMove(){
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED
