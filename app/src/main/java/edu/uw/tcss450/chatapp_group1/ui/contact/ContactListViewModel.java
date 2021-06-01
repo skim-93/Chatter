f8cdd014c24ffb9d1c6e79a442eff1d3b93b1990
Joseph Yun <39817074+josephyunzz@users.noreply.github.com>
2021-05-31 21:01:10 -0700

    Contacts view model uses /chatmembers endpoint for adding chat members

--- app/src/main/java/edu/uw/tcss450/chatapp_group1/ui/contact/ContactListViewModel.java
+++ app/src/main/java/edu/uw/tcss450/chatapp_group1/ui/contact/ContactListViewModel.java
@@ -26,6 +26,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import edu.uw.tcss450.chatapp_group1.io.RequestQueueSingleton;
+
 public class ContactListViewModel extends AndroidViewModel {
     /**Initializer for contact list mutable live data**/
     private MutableLiveData<List<Contact>> mFriendContactList;
@@ -83,6 +85,17 @@ public class ContactListViewModel extends AndroidViewModel {
                                        @NonNull Observer<? super List<Contact>> observer) {
         mRequestList.observe(owner, observer);
     }
+
+    /**
+     * Add contact response observer
+     * @param owner life cycle owner
+     * @param observer observer for list<Contact>
+     */
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        mResponse.observe(owner, observer);
+    }
+
     /**
      * Request to get contact friend list from backend
      * @param jwt authorization token
@@ -225,6 +238,50 @@ public class ContactListViewModel extends AndroidViewModel {
      */
     public List<Contact> getList() {
         return this.mSearchContacts.getValue();
+    }
+
+    /**
+     * Add a contact member to the current chat room
+     * @param jwt user jwt
+     * @param chatID chat id of room
+     * @param memberID id of member to add
+     */
+    public void addContactMemberToChat(final String jwt, int chatID, int memberID) {
+        String url = "https://group1-tcss450-project.herokuapp.com/chatmembers/" + chatID  + "/" + memberID;
+        JSONObject body = new JSONObject();
+        try {
+            body.put("memberid", memberID);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Request request = new JsonObjectRequest(
+                Request.Method.PUT,
+                url,
+                body, // the push token in JSON body
+                mResponse::setValue,
+                this::handleAddContactMemberToChatError) {
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+    }
+
+    /**
+     * handle connection error to chat back-end
+     * @param error the error
+     */
+    private void handleAddContactMemberToChatError(final VolleyError error) {
+        Log.e("CONNECTION ERROR", "No Chat Info");
     }
 
 }
